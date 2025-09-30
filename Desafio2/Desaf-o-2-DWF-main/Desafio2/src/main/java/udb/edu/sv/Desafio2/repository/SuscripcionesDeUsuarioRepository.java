0e514ee5f273450abaf2380fd19c040f30ad81fa
Ghost-guss1622 <87097371+Ghost-guss1622@users.noreply.github.com>
2025-09-30 03:04:50 -0600

    Delete Desafio2/Desaf-o-2-DWF-main directory

--- Desafio2/Desaf-o-2-DWF-main/Desafio2/src/main/java/udb/edu/sv/Desafio2/repository/SuscripcionesDeUsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package udb.edu.sv.Desafio2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import udb.edu.sv.Desafio2.SuscripcionesDeUsuario;
-
-public interface SuscripcionesDeUsuarioRepository extends JpaRepository<SuscripcionesDeUsuario, Long> {
-}
